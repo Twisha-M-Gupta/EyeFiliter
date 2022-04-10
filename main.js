5c23c8b486a19598c3951c9adc55003a5d7a4ec2
Twisha-M-Gupta <90875017+Twisha-M-Gupta@users.noreply.github.com>
2022-04-10 16:54:49 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-function preload() {
+REyeX = 0;
+REyeY = 0;
+LEyeX = 0;
+LEyeY = 0;
 
+function preload() {
+    blueEye = loadImage('https://i.postimg.cc/QM4wxFmr/Final-Blue.png');
 }
 
 function setup() {
     canvas = createCanvas(400, 400);
-    canvas.center;
+    canvas.position(570, 250)
 
     video = createCapture(VIDEO);
     video.size(400, 400);
@@ -16,6 +21,8 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 400, 400);
+    image(blueEye, REyeX, REyeY, 25, 25);
+    image(blueEye, LEyeX, LEyeY, 25, 25);
 }
 
 function takePhoto() {
@@ -29,6 +36,10 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        REyeX = results[0].pose.rightEye.x - 10;
+        REyeY = results[0].pose.rightEye.y - 10;
+        LEyeX = results[0].pose.leftEye.x - 10;
+        LEyeY = results[0].pose.leftEye.y - 10;
         console.log("REye x = " + results[0].pose.rightEye.x);
         console.log("REye y = " + results[0].pose.rightEye.y);
         console.log("LEye x = " + results[0].pose.leftEye.x);
